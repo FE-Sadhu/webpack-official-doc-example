e1deeff00e777a808cef3419f6ec87461afe35ed
sadhu <1714222385@qq.com>
2021-04-05 01:36:07 +0800

    feat: 资源管理

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import Egg from './egg.png';
+
 function component() {
   const element = document.createElement('div');
-
-  // - lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // + lodash，现在通过一个 script 引入
+  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // 将图像添加到我们已经存在的 div 中。
+  const myIcon = new Image();
+  myIcon.src = Egg;
+
+  element.appendChild(myIcon);
 
   return element;
 }
