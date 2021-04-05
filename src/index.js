9aa1ed1d43a92e01a0af55682adfbe6480cf0913
sadhu <1714222385@qq.com>
2021-04-05 14:49:45 +0800

    feat: 管理输出

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Egg from './egg.png';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
-  // Lodash, now imported by this script
+  const btn = document.createElement('button');
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Egg;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource', // webpack 5 内置的 Assets Modules
-      },
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // 每次构建前清理 /dist 文件夹，这样只会生成用到的文件
   },
 };
