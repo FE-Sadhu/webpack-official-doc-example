32906a3ec650a5e7861d4113902ad055c428f3c2
sadhu <1714222385@qq.com>
2021-04-05 23:10:17 +0800

    feat: 手动配置 entry & 防止重复

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-https://webpack.docschina.org/guides/getting-started/
+代码分割三种方式：
+1. 入口起点：使用 entry 配置手动地分离代码。
+2. 防止重复：使用 Entry dependencies 或者 SplitChunksPlugin 去重和分离 chunk。
+3. 动态导入：通过模块的内联函数调用来分离代码。
 
-每个分支对应每个章节
+#### 入口起点的隐患
+1. 如果入口 chunk 之间包含一些重复的模块，那些重复模块都会被引入到各个 bundle 中。
+2. 这种方法不够灵活，并且不能动态地将核心应用程序逻辑中的代码拆分出来。
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    another: './src/another-module.js',
+    /* 使用 SplitChunksPlugin代替如下操作  -> 可以将公共的依赖模块提取到已有的入口 chunk 中，或者提取到一个新生成的 chunk。
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared',
+    },
+    another: {
+      import: './src/another-module.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash', // 在多个 chunk 只引一次这个模块，防止重复
+    */
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    // runtimeChunk: 'single', // 如果要在一个 HTML 页面上使用多个入口时配置
+    splitChunks: {
+      chunks: 'all', // 可以将公共的依赖模块提取到已有的入口 chunk 中，或者提取到一个新生成的 chunk。
+    },
   },
 };
